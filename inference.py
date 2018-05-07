cb27921ac2af1c690dfb17d681d8428cdff1ea41
otenim <snake_istobelieve@keio.jp>
2018-05-08 04:37:25 +0900

    add inference script

--- inference.py
+++ inference.py
@@ -1,52 +1,39 @@
-from keras.applications.xception import Xception
-from keras.applications.inception_v3 import InceptionV3
-from keras.applications.xception import preprocess_input, decode_predictions
+from keras.applications.xception import (
+    Xception, preprocess_input, decode_predictions
+)
 from keras.preprocessing import image
-from keras.models import Model, load_model
+from keras.models import load_model
 import numpy as np
 import os
 import argparse
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    'model',
-)
-parser.add_argument(
-    'image',
-)
-parser.add_argument(
-    '--top',
-    type=int,
-    default=10,
-)
-parser.add_argument(
-    '--classes',
-    default=os.path.join(current_directory, 'classes.txt'),
-)
+parser.add_argument('model')
+parser.add_argument('classes')
+parser.add_argument('image')
+parser.add_argument('--top_n', type=int, default=10)
 
 def main(args):
 
     # create model
     model = load_model(args.model)
 
-    # create classes
+    # load class names
     classes = []
     with open(args.classes, 'r') as f:
         classes = list(map(lambda x: x.strip(), f.readlines()))
 
-    # load image
+    # load an input image
     img = image.load_img(args.image, target_size=(299, 299))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
     # predict
-    preds = model.predict(x)
-    pred = preds[0]
+    pred = model.predict(x)[0]
     result = [(classes[i], float(pred[i]) * 100.0) for i in range(len(pred))]
     result.sort(reverse=True, key=lambda x: x[1])
-    for i in range(args.top):
+    for i in range(args.top_n):
         (class_name, prob) = result[i]
         print("Top %d ====================" % (i + 1))
         print("Class name: %s" % (class_name))
